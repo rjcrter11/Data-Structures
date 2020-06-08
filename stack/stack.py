8c91f027d9e67579c76ca5a988bb419b7e0fbecd
Rachel <rjcrter11@gmail.com>
2020-06-08 14:14:12 -0500

    queue and stack classes pass tests with arrays as storage stucture

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,23 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        storage = self.storage
+        return storage.append(value)
 
     def pop(self):
-        pass
+        storage = self.storage
+        if len(storage) > 0:
+            return storage.pop()
+        else:
+            return None
