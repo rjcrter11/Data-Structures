95452e4a584483e0cf5f6addc0f5084cfb40a1ff
Rachel <rjcrter11@gmail.com>
2020-06-08 15:21:04 -0500

    adds LinkedList code snippet, queue passes tests with LinkedList storage structure

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,103 @@
+
+
+# import sys
+# sys.path.append('../singly_linked_list ')
+# #from singly_linked_list import LinkedList  # noqa
+# #sys.path.insert(1, '../singly_linked_list')
+# from singly_linked_list import LinkedList  # noqa
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+# my_node = Node(1, Node(new node))
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value, None)
+        # LL is empty if head is none
+        # so check if there is no head
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            # LL w/ 1 item, head and tail are the same
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def remove_head(self):
+        # must check if there's a head
+        if not self.head:
+            return None
+        # if head has no next value, there is a single element in the list
+        if not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
+        value = self.head.get_value()
+        # set the head reference to the current head's next node in the list
+        self.head = self.head.get_next()
+        return value
+
+    def contains(self, value):
+        if not self.head:
+            return False
+        # Recursive solution
+        # def search(node):
+           # if node.get_value() == value:
+            # return True
+            # if not node.get_next():
+            # return False
+            # return search(node.get_next())
+        # return search(self.head)
+       # get a reference to the node we're currently at; update this as we traverse the list
+        current = self.head
+        # check to see if we're at a valid node
+        while current:
+            # return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
+                return True
+            # update our current node to the current node's next node
+            current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
+        return False
+
+    def get_max(self):
+        if not self.head:
+            return None
+        # reference to the largest value we've seen so far
+        max_value = self.head.get_value()
+        # reference to our current node as we traverse the list
+        current = self.head.get_next()
+        # check to see if we're still at a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.get_value() > max_value:
+                # if so, update our max_value variable
+                max_value = current.get_value()
+            # update the current node to the next node in the list
+            current = current.get_next()
+        return max_value
+
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -12,21 +112,41 @@ return elements in Last In First Out order.
 """
 
 
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         storage = self.storage
+#         return storage.append(value)
+
+#     def pop(self):
+#         storage = self.storage
+#         if len(storage) > 0:
+#             return storage.pop()
+#         else:
+#             return None
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def push(self, value):
-        storage = self.storage
-        return storage.append(value)
+        self.storage.add_to_tail(value)
+        self.size = + 1
+        return value
 
     def pop(self):
         storage = self.storage
-        if len(storage) > 0:
-            return storage.pop()
+        if self.size != 0:
+            self.size -= 1
+            return storage.remove_head()
         else:
             return None
